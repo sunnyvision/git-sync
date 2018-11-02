f2af91f19af6580bfd2656addc87f2acabb3cf69
Michael HY Tam <michael@sunnyvision.com>
2018-11-02 14:40:03 +0800

    added sync.sh

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This scripts shared an amount of parameters similar to kubenetes/git-sync
+# As original repo has couple of issue on centos host, this is used instead for workaround
+ 
+set -e
+
+# The parameters mainly consumes from environment
